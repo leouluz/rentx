a5cc5d907dce58d97d9f260817e4cc1a0aa77dc2
Leonardo Luz <39962306+leouluz@users.noreply.github.com>
2022-04-02 18:03:41 -0300

    added react-native-svg and created simple header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-import { Container, Title } from "./styles";
+import { 
+  Container, 
+  Title,
+  Header,
+} from "./styles";
+
+import Logo from "../../assets/logo.svg"
+
+import { StatusBar } from "react-native";
 
 export function Home() {
   return (
     <Container>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <Logo />
+      </Header>
       <Title>Home</Title>
     </Container>
   );
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,11 +2,16 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  justify-content: center;
-  align-items: center;
-  background-color: ${({ theme }) => theme.colors.main};
 `;
+
 export const Title = styled.Text`
   font-size: 16px;
   font-family: ${({ theme }) => theme.fonts.secondary_600};
+`;
+
+export const Header = styled.Text`
+  width: 100%;
+  height: 113px;
+
+  background-color: ${({ theme }) => theme.colors.header};
 `;
