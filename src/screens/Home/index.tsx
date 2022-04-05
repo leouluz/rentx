bbdec6f7472f1bf30a4a930880f9623b68458a71
Leonardo Luz <39962306+leouluz@users.noreply.github.com>
2022-04-05 16:21:06 -0300

    created cardCar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -10,8 +10,20 @@ import Logo from "../../assets/logo.svg"
 
 import { StatusBar } from "react-native";
 import { RFValue } from "react-native-responsive-fontsize";
+import { Car } from "../../components/Car";
 
 export function Home() {
+
+  const carData = {
+    brand: 'Audi',
+    name: 'RS 5 Coubé',
+    rent: {
+      period: 'Ao Dia',
+      price: '120',
+    },
+    thumbnail: 'https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png',
+  }
+
   return (
     <Container>
       <StatusBar
@@ -28,6 +40,7 @@ export function Home() {
           <TotalCars>Total de 12 carros</TotalCars>
         </HeaderContent>
       </Header>
+      <Car data={carData} />
     </Container>
   );
 }
